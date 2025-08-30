696a57aa318469af7a8b26ac7d739db9fcf36dab
vivekGithub43 <vivekcherry143@gmail.com>
2025-08-30 10:37:28 +0530

    send

--- routers/authRouter.js
+++ routers/authRouter.js
@@ -7,8 +7,8 @@ const router = express.Router();
 router.post('/signUp',signUp);
 router.post('/signin',signin);
 router.post('/signout',identifier,signout);
-router.patch('/send-verification-code',identifier,sendVerificationCode);
-router.patch('/verify-verification-code',identifier,verifyVerificationCode);
+router.patch('/send-verification-code',sendVerificationCode);
+router.patch('/verify-verification-code',verifyVerificationCode);
 router.patch('/change-password',identifier,changePassword);
 router.patch('/send-forgot-password-code',sendForgotPasswordCode);
 router.patch('/verify-forgot-password-code',verifyForgotPasswordCode)
